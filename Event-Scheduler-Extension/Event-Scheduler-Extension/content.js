8a1c2a388940aaf514c9d089382b33e27c02a742
REYMONSELVAKUMAR G M <145462361+REYMONSELVAKUMAR@users.noreply.github.com>
2025-03-25 17:21:16 +0530

    Delete Event-Scheduler-Extension/Event-Scheduler-Extension directory

--- Event-Scheduler-Extension/Event-Scheduler-Extension/content.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.local.get("selectedText", (data) => {
-        if (data.selectedText) {
-            document.getElementById("eventDetails").innerText = data.selectedText;
-        }
-    });
-});
